57cf1036b1c865316c1826f8768a63ec6be378ef
Clint Newsom <hcnewsom@gmail.com>
2012-11-01 17:16:11 -0400

    updating handle bars tasks

--- public/js/handlebars.helpers.js
+++ public/js/handlebars.helpers.js
@@ -8,3 +8,9 @@ Handlebars.registerHelper('ifnotme', function(me, author, options){
 		return options.fn(this);
 	};
 });
+
+Handlebars.registerHelper('ifisme', function(me, author, options){
+	if(me._id === author){
+		return options.fn(this);
+	};
+});
